8c1a138a3229c885e7a54ef9b15ee0ae8891bb09
Christelle Souka <christelle.souka@etudiant.univ-reims.frgit config --global user.email christelle.souka@etudiant.univ-reims.fr>
2023-10-16 14:40:39 +0200

    Paramètre times rendu optionnel et égal à 3

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -23,7 +23,7 @@ class HelloController extends AbstractController
     }
 
     #[Route('/hello/{name}/{times}', name: 'app_manyTimes', requirements: ['times' =>'\d+'])]
-    public function manyTimes(string $name, int $times): Response
+    public function manyTimes(string $name, int $times = 3): Response
     {
         if (!($times !== 0 && $times <= 10))
             {
