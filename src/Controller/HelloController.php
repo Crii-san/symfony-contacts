c13e2d2e07ef64b0763bdbfe083546f3448ec20c
Christelle Souka <christelle.souka@etudiant.univ-reims.frgit config --global user.email christelle.souka@etudiant.univ-reims.fr>
2023-10-18 11:15:41 +0200

    Ajout lien vers 'Hello Joe 5'

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-    #[Route('/hello', name: 'app_hello')]
+    #[Route('/', name: 'blog_index')]
     public function index(): Response
     {
         return $this->render('hello/index.html.twig');
