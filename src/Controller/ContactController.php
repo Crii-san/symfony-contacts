ed9051ab0dfbc2ab8321edc7894d89abeb2a364c
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-10-22 23:23:09 +0200

    Demande du service « ContactRepository »

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(ContactRepository $contactRepository): Response
     {
         return $this->render('contact/index.html.twig');
     }
