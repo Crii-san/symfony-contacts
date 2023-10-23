05f5b12e4b29b66ff2f91f5fcf2db0796f13a05d
Christelle Souka <christelle.souka@etudiant.univ-reims.frgit config --global user.email christelle.souka@etudiant.univ-reims.fr>
2023-10-23 15:20:48 +0200

    Demande du rendu de la vue pour l'action show

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,7 +16,10 @@ class ContactController extends AbstractController
 
         return $this->render('contact/index.html.twig', ['contacts' => $contacts]);
     }
-    #[Route('/contact/{contactId} ')]
+
+    #[Route('/contact/{contactId}')]
     public function show(int $contactId) : Response
-    {}
+    {
+        return $this->render('contact/index.html.twig');
+    }
 }
