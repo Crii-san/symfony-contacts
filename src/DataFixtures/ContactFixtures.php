f54614a2d3ed9efe28b758926ea347d35849cf23
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-11-05 19:57:04 +0100

    Générateur de données permettant de créer 150 Contact

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,13 +2,19 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 ;
 
 class ContactFixtures extends Fixture
 {
+
     public function load(ObjectManager $manager): void
     {
+        for ($i = 0; $i < 150; $i++)
+        {
+            ContactFactory::createOne();
+        }
     }
 }
