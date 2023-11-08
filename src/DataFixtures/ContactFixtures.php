d8d5b2c0a5a23ea4e1468ce6ee480ec0c3a0a2cd
Christelle Souka <christelle.souka@etudiant.univ-reims.frgit config --global user.email christelle.souka@etudiant.univ-reims.fr>
2023-11-08 09:30:38 +0100

    Correction php-cs-fixer

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -5,15 +5,12 @@ namespace App\DataFixtures;
 use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class ContactFixtures extends Fixture
 {
-
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 150; $i++)
-        {
+        for ($i = 0; $i < 150; ++$i) {
             ContactFactory::createOne();
         }
     }
