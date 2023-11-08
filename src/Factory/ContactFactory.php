767126f23bb2ef0e56dbe2105b34a718832b6476
Christelle Souka <christelle.souka@etudiant.univ-reims.frgit config --global user.email christelle.souka@etudiant.univ-reims.fr>
2023-11-08 09:43:34 +0100

    Correction de l'attribut translirator

--- src/Factory/ContactFactory.php
+++ src/Factory/ContactFactory.php
@@ -11,34 +11,35 @@ use Zenstruck\Foundry\RepositoryProxy;
 /**
  * @extends ModelFactory<Contact>
  *
- * @method        Contact|Proxy create(array|callable $attributes = [])
- * @method static Contact|Proxy createOne(array $attributes = [])
- * @method static Contact|Proxy find(object|array|mixed $criteria)
- * @method static Contact|Proxy findOrCreate(array $attributes)
- * @method static Contact|Proxy first(string $sortedField = 'id')
- * @method static Contact|Proxy last(string $sortedField = 'id')
- * @method static Contact|Proxy random(array $attributes = [])
- * @method static Contact|Proxy randomOrCreate(array $attributes = [])
+ * @method        Contact|Proxy                     create(array|callable $attributes = [])
+ * @method static Contact|Proxy                     createOne(array $attributes = [])
+ * @method static Contact|Proxy                     find(object|array|mixed $criteria)
+ * @method static Contact|Proxy                     findOrCreate(array $attributes)
+ * @method static Contact|Proxy                     first(string $sortedField = 'id')
+ * @method static Contact|Proxy                     last(string $sortedField = 'id')
+ * @method static Contact|Proxy                     random(array $attributes = [])
+ * @method static Contact|Proxy                     randomOrCreate(array $attributes = [])
  * @method static ContactRepository|RepositoryProxy repository()
- * @method static Contact[]|Proxy[] all()
- * @method static Contact[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static Contact[]|Proxy[] createSequence(iterable|callable $sequence)
- * @method static Contact[]|Proxy[] findBy(array $attributes)
- * @method static Contact[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
- * @method static Contact[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Contact[]|Proxy[]                 all()
+ * @method static Contact[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Contact[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Contact[]|Proxy[]                 findBy(array $attributes)
+ * @method static Contact[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Contact[]|Proxy[]                 randomSet(int $number, array $attributes = [])
  */
 final class ContactFactory extends ModelFactory
 {
     protected \Transliterator $transliterator;
+
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
      *
      * @todo inject services if required
      */
-    public function __construct($transliterator)
+    public function __construct()
     {
-        $this->transliterator = $transliterator;
         parent::__construct();
+        $this->transliterator = \Transliterator::create('Any-Latin; Latin-ASCII');
     }
 
     /**
@@ -79,8 +80,7 @@ final class ContactFactory extends ModelFactory
 
     protected function normalizeName(string $name): string
     {
-        $name = $this->transliterator->transliterate($name);
-        $name = preg_replace('/[^a-zA-Z]/', '-', $name);
-        return $name;
+        preg_replace('/[^a-zA-Z]/', '-', $name);
+        return $this->transliterator->transliterate($name);
     }
 }
