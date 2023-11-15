ad3dbb5c5e57fbcedd61f246c539832dc29ec0ec
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-11-15 21:18:37 +0100

    Utilisateur d'un paramètre GET pour la recherche d'un contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(ContactRepository $contactRepository): Response
+    public function index(ContactRepository $contactRepository, Request $request): Response
     {
-        $contacts = $contactRepository->search();
+        $searchText = $request->query->get('search', '');
+        $contacts = $contactRepository->search($searchText);
 
         return $this->render('contact/index.html.twig', ['contacts' => $contacts]);
     }
