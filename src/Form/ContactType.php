80fd5880b0181923aeaaa473fea0bff5b2d55677
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-12-05 21:19:04 +0100

    Précision de la propriété de l'entité « Category »

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Contact;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +18,10 @@ class ContactType extends AbstractType
             ->add('lastname')
             ->add('email')
             ->add('phone')
-            ->add('category')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
         ;
     }
 
