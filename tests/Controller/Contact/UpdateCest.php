95e46408bb3c30c68caf74a7b4ffd3372cf457cc
Christelle Souka <christelle.souka@etudiant.univ-reims.frgit config --global user.email christelle.souka@etudiant.univ-reims.fr>
2023-12-20 11:26:28 +0100

    Mise à jour des tests : ajout d'une connection comme administrateur avant de modifier un contact

--- tests/Controller/Contact/UpdateCest.php
+++ tests/Controller/Contact/UpdateCest.php
@@ -3,12 +3,17 @@
 namespace App\Tests\Controller\Contact;
 
 use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class UpdateCest
 {
-    public function form(ControllerTester $I): void
+    public function formShowsContactDataBeforeUpdating(ControllerTester $I): void
     {
+        $user = UserFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+
         ContactFactory::createOne([
             'firstname' => 'Homer',
             'lastname' => 'Simpson',
