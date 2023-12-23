0c521d567cd9c617722f8c27a0e2ca996cf72322
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-12-23 12:43:18 +0100

    Affichage des catégories triées dans le formulaire

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
 use App\Entity\Contact;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,6 +18,23 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            'firstname',
+            'lastname',
+            'email',
+            'phone',
+            AssociationField::new('category')
+                ->setFormTypeOptions([
+                    'choice_label' => 'name',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('c')
+                            ->orderBy('c.name', 'ASC');
+                    },
+                ]),
+        ];
+    }
     /*
     public function configureFields(string $pageName): iterable
     {
