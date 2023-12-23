71eb94ad5af0ac2777887378159e9b10baa001b4
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-12-23 17:54:02 +0100

    Implémentation de la méthode updateEntity pour la modification du mot de passe d'un User

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,18 +3,27 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
     }
 
     public function configureFields(string $pageName): iterable
@@ -30,11 +39,24 @@ class UserCrudController extends AbstractCrudController
                 'required' => false,
                 'empty_data' => '',
                 'attr' => [
-                    'autocomplete' => 'off'
-                ]
+                    'autocomplete' => 'off',
+                ],
             ]),
             ArrayField::new('roles'),
         ];
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $request = $this->getContext()->getRequest();
+        $password = $request->get('User')['password'] ?? null;
+
+        if (!empty($password)) {
+            $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $password);
+            $entityInstance->setPassword($hashedPassword);
+        }
+
+        parent::updateEntity($entityManager, $entityInstance);
     }
 
     /*
