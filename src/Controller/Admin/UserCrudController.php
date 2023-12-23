45e57058c878f33faff34a733f80a95211850d2b
Christelle Souka <christelle.souka@etudiant.univ-reims.fr>
2023-12-23 17:01:58 +0100

    Configuration des champs pour l'entité User

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,15 +4,37 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            TextField::new('email'),
+            TextField::new('password')->onlyOnForms()
+            ->setFormType(PasswordType::class)
+            ->setFormTypeOptions([
+                'required' => false,
+                'empty_data' => '',
+                'attr' => [
+                    'autocomplete' => 'off'
+                ]
+            ]),
+            ArrayField::new('roles'),
+        ];
     }
 
     /*
